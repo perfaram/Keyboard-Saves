591f49fcc110b1ff9520f1f423adf684d86a6dde
perfaram <perfaram@windowslive.com>
2013-11-13 15:48:04 +0100

    Update keysave.php

--- keysave.php
+++ keysave.php
@@ -1,10 +1,10 @@
 <?php
 /*
 Plugin Name: Keyboard Saves
-Plugin URI: http://leapxl.com/dev/wordpress
-Description: Enregistrement clavier
-Author: Paul KILGOUR
-Version: 1.0
+Plugin URI: http://dev.leapxl.com/wordpress
+Description: Allows to save with CTRL-S in WordPress
+Author: Perceval FARAMAZ
+Version: 1.1
 Author URI: http://me.leapxl.com
 */
 ?>
@@ -19,25 +19,25 @@ if( is_admin() ) {
 	);
 	
 	foreach($button_to_click as $page => $button_id) {
-		add_action( 'admin_footer-'.$page, 'swk_add_script' );
+		add_action( 'admin_footer-'.$page, 'lpx_add_script' );
 	}
 }
 
-function swk_add_script() {
+function lpx_add_script() {
 	global $pagenow, $button_to_click;
 	?>
 	<script>
 		var button_id = "<?php echo $button_to_click[$pagenow]; ?>";
 		
-		jQuery('#'+button_id).attr('title', 'Ctrl+S or Cmd+S to click');
+		jQuery('#'+button_id).attr('title', 'Ctrl+S or Cmd+S to save');
 		
 		jQuery(document).keydown( function(e) {
 			if( (e.keyCode || e.which) == 83 && (e.ctrlKey || e.metaKey) ) {
 				jQuery('#'+button_id).click();
-				
 				return false;
 			}
 		});
 	</script>
 <?php
 }
+?>
